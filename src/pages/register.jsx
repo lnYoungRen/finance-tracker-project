e16d55d261e88a37548d63b9fbe85db8eb4dd7f1
InYoungRen <srjsu4@gmail.com>
2024-11-29 22:58:16 -0800

    updated register url

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const BACKEND = "https://finance-tracker-project.onrender.com";
+const BACKEND = "https://finance-tracker-project.onrender.com/auth/register";
 
 
 function RegisterForm() {
